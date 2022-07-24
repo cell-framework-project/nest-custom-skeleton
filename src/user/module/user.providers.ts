b50519e5a5f405f1d46115d4af6ab4b28290d9c4
cell-framework-project <bernardocoloradodubois.fsd@gmail.com>
2022-07-23 23:09:38 -0500

    refactorizando jwt

--- src/user/module/user.providers.ts
+++ src/user/module/user.providers.ts
@@ -2,8 +2,6 @@ import { DataSource } from 'typeorm';
 import { User } from '../domain.model/user';
 import { UserRepository } from '../domain.model/user.repository';
 
-
-
 export const userProviders = [
   {
     provide: 'USER_REPOSITORY',
