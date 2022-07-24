454891c81bde9136672900b701753f16665abcf8
cell-framework-project <bernardocoloradodubois.fsd@gmail.com>
2022-07-24 16:16:51 -0500

    agregando pipes a controladores

--- src/user/controller/user.hexagonal.signin.controller.ts
+++ src/user/controller/user.hexagonal.signin.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import { Controller, Body, Post, ValidationPipe } from '@nestjs/common';
 import { UserEmailAuthenticateDto } from '../dto/user.email.authenticate.dto';
 import { UserEmail } from '../domain.model/user.email';
 import { UserPassword } from '../domain.model/user.password';
 import { UserAuthService } from '../application.service/user.auth.service';
 
+
 @Controller('user-hex')
 export class UserHexagonalSigninController {
-constructor(private readonly userAuthService:UserAuthService) {}
+constructor( private readonly userAuthService:UserAuthService ) {}
 
   @Post('signin')
-  signin(@Body() userAuthDto: UserEmailAuthenticateDto){
+  signin(@Body( new ValidationPipe() ) userAuthDto: UserEmailAuthenticateDto){
 
     const email:UserEmail = UserEmail.create(userAuthDto.email);
     const password:UserPassword = new UserPassword(userAuthDto.password);
