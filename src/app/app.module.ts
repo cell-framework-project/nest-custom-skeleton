6028ef2d8675e046378756776e88edaa59cb56e1
cell-framework-project <bernardocoloradodubois.fsd@gmail.com>
2022-07-24 20:06:33 -0500

    agregando pipes a controladores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '../app/app.controller';
 import { AppService } from '../app/app.service';
-import { UserHexagonalModule } from '../user/module/user.module';
+import { UserModule } from '../user/module/user.module';
 
 @Module({
   imports: [UserModule],
