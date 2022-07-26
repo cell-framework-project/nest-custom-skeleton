44c0c0ed27504eac1fc2036671df22700b235579
cell-framework-project <bernardocoloradodubois.fsd@gmail.com>
2022-07-26 13:58:17 -0500

    revisando repositorio completo

--- src/config/database.constants.ts
+++ src/config/database.constants.ts
@@ -8,4 +8,4 @@ export const databaseConstants = {
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME
-}
+};
--- src/config/jwt.constants.ts
+++ src/config/jwt.constants.ts
@@ -3,6 +3,6 @@ import * as dotenv from 'dotenv';
 const config = dotenv.config({path:__dirname+'/../../.env'});
 
 export const jwtConstants = {
-  secretKey: process.env.JWT_SECRET_KEY, 
-  expiresIn: process.env.JWT_EXPIRES_IN 
-}
+  secretKey: process.env.JWT_SECRET_KEY,  
+  expiresIn: process.env.JWT_EXPIRES_IN  
+};
