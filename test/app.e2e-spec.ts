3f50886141ee5696efa30164a56402909cd42342
cell-framework-project <bernardocoloradodubois.fsd@gmail.com>
2022-07-26 14:04:09 -0500

    revisando repositorio completo

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,6 +6,7 @@ import { AppModule } from 'src/app/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
+  //
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -15,6 +16,7 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  //
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
