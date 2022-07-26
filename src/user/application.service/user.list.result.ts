9c31163d80d3ab44b0251ceef624cf12cbc1d9c9
cell-framework-project <bernardocoloradodubois.fsd@gmail.com>
2022-07-25 19:50:04 -0500

    limpiados objetos obsoletos

---
+++ src/user/application.service/user.list.result.ts
@@ -0,0 +1,28 @@
+import { User } from 'src/user/domain.model/user';
+
+export class UserListResult{
+
+  readonly viewModel:object[];
+
+  constructor(users:User[]){
+
+    const viewModel=[];
+    users.forEach((user:User)=>{
+      const userViewModel={
+        id:user.id,
+        nickname:user.nickname.value,
+        email:user.email.value,
+        name:{
+          first:user.name.first,
+          last:user.name.last
+        },
+        creationDateTime:user.creationDateTime
+      };
+    viewModel.push(userViewModel);
+    });
+    
+    this.viewModel=viewModel;
+    
+  }
+
+}
