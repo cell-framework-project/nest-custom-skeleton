1aeac64a2c6aa92ad71335bd0c5b8c7cf988c7e5
cell-framework-project <bernardocoloradodubois.fsd@gmail.com>
2022-07-27 14:55:23 -0500

    cambiando eventos de logeo

---
+++ src/user/cqrs/event/user.login.succeed.event.ts
@@ -0,0 +1,33 @@
+import { User } from "../../domain.model/user";
+import {v4 as uuid4} from 'uuid';
+
+export class UserLoginSuccedEvent{
+
+  public readonly id:string;
+  public readonly key:string='user.login.failed'
+  public readonly data:object;
+  public readonly dateTime:Date;
+
+  constructor(user:User){
+
+    this.id=uuid4();
+
+    this.data={
+
+      user:{
+        id:user.id,
+        nickname:user.nickname.value,
+        email:user.email.value,
+        name:{
+          first:user.name.first,
+          last:user.name.last
+        },
+        creationDateTime:user.creationDateTime
+      },
+
+    }
+    
+    this.dateTime = new Date();
+  }
+
+}
