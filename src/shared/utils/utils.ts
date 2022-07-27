a3a011ff46412e8b4fd27757709e978dd126fc2c
cell-framework-project <bernardocoloradodubois.fsd@gmail.com>
2022-07-27 15:38:21 -0500

    modulo de utilidades actualizado

--- src/shared/utils/utils.ts
+++ src/shared/utils/utils.ts
@@ -7,20 +7,20 @@ export function randomCodeGenerator(length:number):string{
   for (let index = 0; index < length; index++) {
     code += Math.round(Math.random()*10).toString(); 
   }
+
   return code;
   
 }
 
 export function uuid4(){
-
   return v4();
-
 }
 
-
-export function fixedLengthIntegerString(int:number,length:number){
+export function fixedLengthIntegerString(int:number,length:number):string{
 
   const stringInt = int.toString();
-  const repeat = length - stringInt.length;
+  const count = length - stringInt.length;
+
+  return '0'.repeat(count) + stringInt;
 
 }
