eb105020bf7b5c2984b8445a2c34f19b4c6c0f79
cell-framework-project <bernardocoloradodubois.fsd@gmail.com>
2022-07-27 13:32:13 -0500

    cambiando estrructura de cvarpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UtilsModule } from 'src/utils/module/utils.module';
-import { AppController } from '../app/app.controller';
-import { AppService } from '../app/app.service';
+import { AppController } from 'src/app/user.interface/controller/app.controller';
+import { AppService } from './application.service/app.service';
 import { UserModule } from '../user/module/user.module';
 
 @Module({
