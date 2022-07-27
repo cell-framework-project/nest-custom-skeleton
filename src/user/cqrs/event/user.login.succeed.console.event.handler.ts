3726454acacf13338b4f3046398f4d82d392cad3
cell-framework-project <bernardocoloradodubois.fsd@gmail.com>
2022-07-27 15:12:08 -0500

    separando eventos de logeo

--- src/user/cqrs/event/user.login.succeed.console.event.handler.ts
+++ src/user/cqrs/event/user.login.succeed.console.event.handler.ts
@@ -2,7 +2,7 @@ import { UserLoginSucceedEvent } from "./user.login.succeed.event";
 import { EventsHandler, IEventHandler } from "@nestjs/cqrs";
 
 @EventsHandler(UserLoginSucceedEvent)
-export class UserLoginFailedConsoleEventHandler implements IEventHandler<UserLoginSucceedEvent> {
+export class UserLoginSucceedConsoleEventHandler implements IEventHandler<UserLoginSucceedEvent> {
 
   handle(event: UserLoginSucceedEvent) {
     console.log(event);
